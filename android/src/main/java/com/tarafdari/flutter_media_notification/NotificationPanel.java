463a6316f5e224269c21de7fb236d51b45477594
Ali Yazdi <ma.yazdi75@gmail.com>
2019-07-24 15:53:42 +0430

    supported androidX

--- android/src/main/java/com/tarafdari/flutter_media_notification/NotificationPanel.java
+++ android/src/main/java/com/tarafdari/flutter_media_notification/NotificationPanel.java
@@ -5,9 +5,9 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationCompat.Style;
 import android.widget.RemoteViews;
+
+import androidx.core.app.NotificationCompat;
 
 class NotificationPanel {
     private Context parent;
